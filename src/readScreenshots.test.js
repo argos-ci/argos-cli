c0074d03dd3366a2932566ca27ffdd51778db4dc
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-04-24 18:59:47 +0200

    feat(name): send the name of the screenshots to the service

--- src/readScreenshots.test.js
+++ src/readScreenshots.test.js
@@ -7,8 +7,14 @@ describe('readScreenshots', () => {
       cwd: path.join(__dirname, '../__fixtures__/screenshots'),
     })
     expect(screenshots).toEqual([
-      path.resolve(__dirname, '../__fixtures__/screenshots/nested/alicia.jpg'),
-      path.resolve(__dirname, '../__fixtures__/screenshots/penelope.jpg'),
+      {
+        name: 'nested/alicia.jpg',
+        filename: path.resolve(__dirname, '../__fixtures__/screenshots/nested/alicia.jpg'),
+      },
+      {
+        name: 'penelope.jpg',
+        filename: path.resolve(__dirname, '../__fixtures__/screenshots/penelope.jpg'),
+      },
     ])
   })
 
@@ -17,8 +23,9 @@ describe('readScreenshots', () => {
       cwd: path.join(__dirname, '../__fixtures__/screenshots'),
       ignore: ['**/alicia.jpg'],
     })
-    expect(screenshots).toEqual([
-      path.resolve(__dirname, '../__fixtures__/screenshots/penelope.jpg'),
-    ])
+    expect(screenshots).toEqual([{
+      name: 'penelope.jpg',
+      filename: path.resolve(__dirname, '../__fixtures__/screenshots/penelope.jpg'),
+    }])
   })
 })
